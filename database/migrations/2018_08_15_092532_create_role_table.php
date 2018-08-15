e85b6316d41fcc466bd66180a1fe8f6c54da6674
raoyc <raoyc@foxmail.com>
2018-08-15 17:34:46 +0800

    create migrations files

---
+++ database/migrations/2018_08_15_092532_create_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 40)->unique('roles_name_unique');
+            $table->string('display_name', 80)->nullable();
+            $table->string('description', 160)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
