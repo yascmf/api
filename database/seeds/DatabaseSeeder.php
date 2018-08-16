ea349fbd4d6b58644628b2fb3eaca2af6cf8bd4b
raoyc <raoyc@foxmail.com>
2018-08-16 15:14:33 +0800

    seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        $this->call('SystemOptionsSeeder');
+        $this->call('UserRolePermissionSeeder');
+        $this->call('CategoriesAndArticlesSeeder');
     }
 }
